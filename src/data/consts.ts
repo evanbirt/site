e466489a218f91430634cc2e4e3fdb1dba63c2b8
Evan Birt <evan@birt.sh>
2023-03-06 14:57:37 -0700

    chore: update meta

--- src/data/consts.ts
+++ src/data/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "My personal website.";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Evan Birt's site";
+export const SITE_DESCRIPTION = "I care deeply, I help solve stuff";
+export const COPYRIGHT = "Evan Birt";
+export const LINKEDIN_PROFILE = "https://www.linkedin.com/in/evanbirt/";
+export const GITHUB_PROFILE = "https://github.com/evanbirt/";
+export const RSS_FEED = "/rss.xml";
