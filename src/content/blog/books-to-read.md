8ad1eab0bfad70f595fa9e14492e4dff44696749
Evan Birt <evan@birt.sh>
2023-03-06 20:21:47 -0700

    chore: update blog

---
+++ src/content/blog/books-to-read.md
@@ -0,0 +1,16 @@
+---
+title: "Books to read"
+description: "The books to read and recommend"
+pubDate: "Dec 01 2022"
+updatedDate: "Jan 01 2023"
+---
+
+## tech
+
+"Unix® and Linux® System Administration Handbook" by Evi Nemeth, Garth Snyder, Trent R. Hein, Ben Whaley, Dan Mackin
+
+"Don't Make Me Think" by Steve Krug
+
+## life
+
+"Ikigai: The Japanese Secret to a Long and Happy Life" by Héctor García, Francesc Miralles
--- src/data/consts.ts
+++ src/data/consts.ts
@@ -1,29 +1,31 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Evan Birt.";
+export const SITE_TITLE = "Evan Birt";
 export const SITE_DESCRIPTION = "I care deeply, I help solve stuff";
+export const BLOG_TITLE = "Blog";
+export const BLOG_DESCRIPTION = "Ramblings by Evan Birt";
 export const COPYRIGHT = "Evan Birt";
 export const RSS_FEED = "/rss.xml";
 export const FOOTER_LINKS = [
   {
     words: "my",
-    href: "https://github.com/evanbirt/",
+    href: "https://github.com/evanbirt",
     link: "github",
   },
   {
     words: "my",
-    href: "https://www.linkedin.com/in/evanbirt/",
+    href: "https://www.linkedin.com/in/evanbirt",
     link: "linkedin",
   },
   {
     words: "astro",
-    href: "https://astro.build/",
+    href: "https://astro.build",
     link: "docs",
   },
   {
     words: "blog",
-    href: "https://github.com/evanbirt/site/",
+    href: "https://github.com/evanbirt/site",
     link: "source",
   },
 ];
